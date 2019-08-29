206fa97e124d1fa499c4de59a48ee7a868908837
iampatik <janpatrick.tancinco@student.passerellesnumeriques.org>
2019-08-29 08:54:36 +0800

    adding an alert

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,59 +2,66 @@ var btnConnect = document.getElementById('connect');
 var btnDisConnect = document.getElementById('disconnect');
 var btnPublish = document.getElementById('publish');
 var btnSubscribe = document.getElementById('subscribe');
+var btnUnSubscribe = document.getElementById('unsubscribe');
 // var result = document.getElementById('result');
 var conRes = document.getElementById('connectresult');
 var topicinput = document.getElementById('topic').value
 var timestamp = new Date()
+var data;
 
 // basic functionalities
  
+alert("PLEASE CONNECT THE BROKER FIRST")
 // var client;// = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
 btnConnect.addEventListener('click', function(e) {
-    //  e.preventDefault();
-    var brokerAdress = document.getElementById('broker').value;
-    console.log(brokerAdress);
-    client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
-    client.on("connect", function(){
-      conRes.value= "Successfully connected";
-      conRes.style.backgroundColor='rgb(0, 204, 0)';
-      conRes.style.color = 'yellow';
-    console.log("Successfully connected");
-})
+        e.preventDefault();
+        var brokerAdress = document.getElementById('broker').value;
+        console.log(brokerAdress);
+        client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
+        client.on("connect", function(){
+          conRes.value= "Successfully connected";
+          conRes.style.backgroundColor='rgb(0, 204, 0)';
+          conRes.style.color = 'yellow';
+        console.log("Successfully connected");
+    })
 })
 
 btnPublish.addEventListener('click',function(e){
-  e.preventDefault();
-  var payload = document.getElementById('payload').value 
-  topic = document.getElementById('topic').value
-  console.log(topic)
-  client.on("message", function (topic, payload) {
-      // var holder = [topic, payload].join(": ");
-      // result.value = holder;
-    var data = "<tr><td>"+topic+"</td>"+
-                "<td>"+payload+"</td>"+
-                "<td>"+timestamp+"</td>"+
-              "</tr>"
-    $('table tbody').append(data);
-  })
-  
-  client.publish("mqtt/"+topic,payload)
+      e.preventDefault();
+      var payload = document.getElementById('payload').value;
+      topic = document.getElementById('topic').value
+      console.log(topic)
+      client.on("message", function () {
+      data = "<tr><td>"+topic+"</td>"+
+            "<td>"+payload+"</td>"+
+            "<td>"+timestamp+"</td>"+
+          "</tr>"
+          $('table tbody').append(data).style.textOverflow = 'ellipsis';
+      })
+            client.publish(topic,payload)
 })
 
 btnSubscribe.addEventListener('click',function(e) {
-  // e.preventDefault();
-  var topic = document.getElementById('topic').value
-  console.log(topic)
-  client.subscribe("mqtt/"+topic)
+      e.preventDefault();
+      var topic = document.getElementById('topic').value
+      console.log(topic)
+      client.subscribe(topic)
 
 })
 
 btnDisConnect.addEventListener('click', function() {
-  console.log(client.end());
-  client.end();
-  conRes.value= "Successfully Disconnected";
-  conRes.style.backgroundColor='rgb(204, 0, 0)';
-      conRes.style.color = 'yellow';
+        console.log(client.end());
+        client.end();
+        conRes.value= "Successfully Disconnected";
+        conRes.style.backgroundColor='rgb(204, 0, 0)';
+            conRes.style.color = 'yellow';
+})
+
+btnUnSubscribe.addEventListener('click',function(e) {
+      e.preventDefault();
+      console.log('unsubscribe')
+      client.unsubscribe(topic,payload);
+  
 })
 
 
